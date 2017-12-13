5d6e1423c59c7b1e5f500c41ec1847497f4efa4a
Kyanite3221 <g.j.b.vanvliet@student.utwente.nl>
2017-12-13 16:21:54 +0100

    added propper IP-ing

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from PublicIP import IP
 app = Flask(__name__)
 
 state = "none"
@@ -10,16 +11,24 @@ def hello():
 @app.route("/smartnotch")
 def serve_smartnotch():
 	#code for serving the html page for smartnotch
+	return "not yet implemented"
 
 @app.route("/majorlaser")
 def serve_majorlaser():
 	#code for serving the html page for major laser
+	return "not yet implemented"
 
 @app.route("/controller")
 def serve_controller():
 	#code for serving the html page for controller
+	return "not yet implemented"
 
 @app.route("/api", methods=["POST"])
 def post_message():
 	#code for handling a post request
 	#state = request.getString() <-- pseudocode
+	return "not yet implemented"
+
+if __name__ == "__main__":
+	print IP()
+	app.run(IP(),5000)
